7706a50c0239e09898b378e3af9f1e8bbeb9299b
Brianna Fredeluces <bfredelu@ucsd.edu>
2021-02-14 22:22:55 -0800

    Submission commit

--- public/js/mapQuest.js
+++ public/js/mapQuest.js
@@ -1,3 +1,12 @@
 function initMap() {
-	// add your code here
+	L.mapquest.key = 'v26b5gKvE2P2U3sJFBZLSD5yPMbC5Q2Q';
+
+	var map = L.mapquest.map('map', {
+		center: [32.88, -117.236],
+		layers: L.mapquest.tileLayer('map'),
+		zoom: 12,
+		zoomControl: false
+	});
+
+	L.marker([32.88, -117.236]).addTo(map);
 }
